b762b8c3d36cf26439c26c839e8be6f1747ecb49
Yu Hin <93305800+yhc369@users.noreply.github.com>
2024-03-25 23:09:14 +0800

    skeleton script

---
+++ Assets/Script/Enemies/Skeleton.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
+
+public class Skeleton : Entity
+{
+    private SpriteRenderer spriteRenderer;
+    private Vector2Int currentDirection= new Vector2Int(1, 0);
+    private Vector2Int faceDirection=new Vector2Int(1, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveStraight(Vector2Int direction)
+    {
+        Tilemap levelTilemap = GameManager.Instance.LevelTilemap;
+
+       
+       
+        if (!EntityManager.Instance.IsPositionBlocked(position + direction))
+        {
+            //rotate
+            if (direction.x == 0)
+            {
+                spriteRenderer.flipX = !spriteRenderer.flipX;
+            }
+            this.currentDirection = new Vector2Int(-direction.x, -direction.y);
+        }
+       
+        GameManager.Instance.AddAction(new MoveAction(this, direction));
+    }
+
+    public override void Action()
+    {
+        MoveStraight(currentDirection);
+    }
+}
